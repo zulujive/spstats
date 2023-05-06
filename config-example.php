39c27d4daa7f4e46b4bbd5b39b59f6a89a4dc1de
zulujive <51298470+zulujive@users.noreply.github.com>
2023-05-05 23:40:17 -0600

    Create config-example.php

---
+++ config-example.php
@@ -0,0 +1,13 @@
+<?php
+	// Name of your site:
+	$site_name = 'STEMPlace';
+	// Set the Password for the Archive Utility:
+	$valid_key = '1234';
+	// The URL of your site (DO NOT add a trailing slash):
+	$site_url = 'https://stemplace.org';
+	// Path to the Archives Page (add a leading slash but trailing slash is optional):
+	$archives_path = '/extra/archives';
+	// Name of the archives file you wish to use
+	// REMEMBER: You MUST have a <-- INSERT CONTENT HERE --> marker in your file for it to function
+	$filename = 'stemplacearchives.php';
+?>
